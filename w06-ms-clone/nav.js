d1ade09affc3a81f3d71ba05f15eee27157517f2
JacobTsai910226 <73874496+JacobTsai910226@users.noreply.github.com>
2020-11-13 22:16:22 +0800

    Add files via upload

---
+++ w06-ms-clone/nav.js
@@ -0,0 +1,45 @@
+function change(index) {
+  const p = document.getElementById('show');
+  console.log('show', p);
+  switch (index) {
+    case 1:
+      p.innerHTML = '<iframe  width=100% height=100% src="./w01/index.html" />';
+      break;
+    case 2:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w01/pokemon.html" />';
+      break;
+    case 3:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w02-imgGallery/imgGallery.html" />';
+      break;
+    case 4:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w02-blog/blog.html" />';
+      break;
+    case 5:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w03-div/div.html" />';
+      break;
+    case 6:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w04-tku60-js/div_tku60.html" />';
+      break;
+    case 7:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w06-ms-clone/index.html" />';
+      break;
+    case 8:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w01/class.html" />';
+      break;
+    case 9:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w01/vitae.html" />';
+      break;
+    case 10:
+      p.innerHTML =
+        '<iframe  width=100% height=100% src="./w01/poject.html" />';
+      break;
+      }
+}
